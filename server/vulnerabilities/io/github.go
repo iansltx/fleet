d98d9c74e3e6ba2849a1957deb9c7ead1a7cbf0f
Ian Littman <iansltx@gmail.com>
2024-09-27 16:42:43 -0500

    Switch back to fleetdm upstream
    Avoids 401s/403s from MSRC, hopefully

--- server/vulnerabilities/io/github.go
+++ server/vulnerabilities/io/github.go
@@ -101,7 +101,7 @@ func (gh GitHubClient) list(ctx context.Context, prefix string, ctor func(fileNa
 
 	releases, r, err := gh.releases.ListReleases(
 		ctx,
-		"iansltx",
+		"fleetdm",
 		"nvd",
 		&github.ListOptions{Page: 0, PerPage: 10},
 	)
